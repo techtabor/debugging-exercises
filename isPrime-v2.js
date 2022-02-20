4c21ed44c3bfe7be2df30f11049bd5aebfb645ff
Ildiko Czeller <czeildi@gmail.com>
2022-02-20 14:14:39 +0100

    add two javscript examples with prime numbers

---
+++ isPrime-v2.js
@@ -0,0 +1,25 @@
+/*
+  Check if a number is prime. Return true if number is prime, false otherwise.
+  number: an integer
+*/
+function isPrime(number) {
+  if (number === 0 || number === 1) {
+    return false;
+  }
+  const checkList = range(2, number - 1);
+  for (check in checkList) {
+    if (number % check === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function range(low, high) {
+  if (high < low) return [];
+  return Array.from(new Array(high - low), (_, i) => i + low);
+}
+
+// test
+
+range(0, 20).forEach(number => console.log(`isPrime(${number}): ${isPrime(number)}`));
