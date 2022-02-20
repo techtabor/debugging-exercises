59a70cf73f26ce24cac9ce2c9ddac9a2b3488ab4
SzG <szgabbor@gmail.com>
2022-02-20 18:00:03 +0100

    refactor magic.js

--- paint2/magic.js
+++ paint2/magic.js
@@ -1,132 +1,125 @@
-var x = (function(){
+const paint = (function () {
 
 	//get the stuff
-	var canvas = document.getElementById("canvas");
+	const canvas = document.getElementById("canvas");
 
-	var context = canvas.getContext("2d");
+	const context = canvas.getContext("2d");
 
 	canvas.height = window.innerHeight;
 	canvas.width = window.innerWidth;
 
-	var brush = document.getElementById("bsize");
-	var clear = document.getElementById("clear");
-	var erase = document.getElementById("erase");
-	var shade = document.getElementsByClassName("shade");
-	var n = shade.length;
+	const clear = document.getElementById("clear");
+	const erase = document.getElementById("erase");
+	const shade = document.getElementsByClassName("shade");
 
-	var inc = document.getElementById("inc");
-	var dec = document.getElementById("dec");
+	const inc = document.getElementById("inc");
+	const dec = document.getElementById("dec");
 
-	var maxSize = 60;
-	var minSize = 5;
+	const maxSize = 60;
+	const minSize = 5;
 
-	var size = 20;
-	var current = document.getElementById("bsize");
-	var save = document.getElementById("save");
+	let size = 20;
+	const current = document.getElementById("bsize");
+	const save = document.getElementById("save");
 
 	//increase size
-	function increase(){
+	function increase() {
 
 		size += 5;
 
-		if(size>maxSize)
+		if (size > maxSize)
 			size = maxSize;
-		else if(size<minSize)
+		else if (size < minSize)
 			size = minSize;
 
 		current.innerHTML = size;
 	}
-	inc.addEventListener("click",increase);
-	
+	inc.addEventListener("click", increase);
+
 	//decrease size
-	function decrease(){
+	function decrease() {
 
 		size -= 5;
 
-		if(size>maxSize)
+		if (size > maxSize)
 			size = maxSize;
-		else if(size<minSize)
+		else if (size < minSize)
 			size = minSize;
-		
-		current.innerHTML = size;	
+
+		current.innerHTML = size;
 	}
-	dec.addEventListener("click",decrease);
+	dec.addEventListener("click", decrease);
 
 	//change color
-	function colorChange(e)
-	{
-		var color = e.target;
+	function colorChange(e) {
+		const color = e.target;
 		context.fillStyle = color.style.backgroundColor;
 		context.strokeStyle = color.style.backgroundColor;
 
-		var active = document.getElementsByClassName("active")[0];
-			console.log(color);
+		const active = document.getElementsByClassName("active")[0];
 		active.className = 'shade';
 
 		color.className += ' active';
 	}
 
-	for(var i =0;i<shade.length;i++)
-	{
-		shade[i].addEventListener("click",colorChange);
+	for (let i = 0; i < shade.length; i++) {
+		shade[i].addEventListener("click", colorChange);
 	}
-	var press = false;
+	let press = false;
 
 	//draw line
-	function drawLine(e){
-		
-		if(press){
-		context.lineTo(e.clientX,e.clientY);
-		context.lineWidth = 2*size;
-		context.stroke();
+	function drawLine(e) {
 
-		context.beginPath();
-		console.log(e.clientX);
-		
-		context.arc(e.clientX,e.clientY,size,0,2*Math.PI);
+		if (press) {
+			context.lineTo(e.clientX, e.clientY);
+			context.lineWidth = 2 * size;
+			context.stroke();
 
-		context.fill();
-		
-		context.beginPath();
-		console.log(e.clientX);
-		
-		context.moveTo(e.clientX,e.clientY);
+			context.beginPath();
+
+			context.arc(e.clientX, e.clientY, size, 0, 2 * Math.PI);
+
+			context.fill();
+
+			context.beginPath();
+
+			context.moveTo(e.clientX, e.clientY);
 		}
 	}
 
-	function up(e){
+	function up(e) {
 		press = false;
 		context.beginPath();
 	}
 
-	function down(e){
+	function down(e) {
 		drawLine(e);
 	}
 
-	canvas.addEventListener('mouseup',up);
-	canvas.addEventListener('mousedown',down);
-	canvas.addEventListener('mousemove',drawLine);
+	canvas.addEventListener('mouseup', up);
+	canvas.addEventListener('mousedown', down);
+	canvas.addEventListener('mousemove', drawLine);
 
 	//erase
-	function erasescr(){
+	function erasescr() {
 
 		context.fillStyle = '#fff';
 		context.strokeStyle = '#fff';
 	}
-	erase.addEventListener("click",erasescr);
+	erase.addEventListener("click", erasescr);
 
 	//clear
-	function clearscr(){
-		context.clearRect(0,0,window.innerHeight,window.innerWidth);
+	function clearscr() {
+		context.clearRect(0, 0, window.innerHeight, window.innerWidth);
 	}
-	clear.addEventListener("click",clearscr);
+	clear.addEventListener("click", clearscr);
 
 	//save
-	function saveFile(e){
+	function saveFile(e) {
 		e.target.href = canvas.toDataURL();
 		e.target.download = "drawing.png";
 	}
 
-	save.addEventListener("click",saveFile);
+	save.addEventListener("click", saveFile);
 })();
 
